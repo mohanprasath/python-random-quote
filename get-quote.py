1611be1ea1fe18412cc0450dab1619fd1e887cc8
Mohan Prasath Chinnasamy <mohanprasath@users.noreply.github.com>
2020-07-04 15:37:10 +0300

    Issue 4 Solved

--- get-quote.py
+++ get-quote.py
@@ -7,9 +7,10 @@ def main():
   quotes = f.readlines()
   f.close()
   # generating a random number and then printing a line at that index in quites 
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  last = random.randint(0, 33)
+  for i in range(0, last):
+    print(quotes[i].rstrip("\n"))
+  # just opena file in append mode and add the line read from the user
 
 if __name__== "__main__":
   main()
